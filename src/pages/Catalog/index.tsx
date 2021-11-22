ace8a1a7cd14b9996b94e81242e0e2f04a082246
janaite <janaite@gmail.com>
2021-11-22 15:23:21 -0300

    page Catalog (incomplete)

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,6 +1,63 @@
+import ProductImage from 'assets/images/car-card.png';
+
+import './styles.css';
 
 const Catalog = () => {
-    return <h1>Catalogo</h1>
+    return (
+        <div className="catalog-container">
+            <div className="catalog-search-container">
+                Digite sua busca
+                <button className="btn btn-primary">Buscar</button>
+            </div>
+            <div className="catalog-card-container">
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div className="catalog-card">
+                    <img src={ProductImage} alt="Nome do produto" />
+                    <h1>Audi Supra TT</h1>
+                    <p>Lorem ipsum sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Catalog;
