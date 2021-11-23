b7106c47a8b2b91d5a3a2fc555ea4f3223143a89
janaite <janaite@gmail.com>
2021-11-23 09:48:53 -0300

    catalog search (incomplete)

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -5,13 +5,25 @@ import './styles.css';
 const Catalog = () => {
   return (
     <div className="catalog-container">
-      <div className="catalog-search-container">
-        Digite sua busca
-        <button className="btn btn-primary">Buscar</button>
-      </div>
-      <div className="catalog-card-container">
-        <div className="container my-4">
+      <div className="container my-4">
+        <div className="catalog-search-container">
+          <input type="text" placeholder="Digite sua busca"/>
+          <button className="btn btn-primary">Buscar</button>
+        </div>
+        <div className="catalog-card-container">
           <div className="row">
+            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+              <ProductCard />
+            </div>
+            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+              <ProductCard />
+            </div>
+            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+              <ProductCard />
+            </div>
+            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+              <ProductCard />
+            </div>
             <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
               <ProductCard />
             </div>
