a46f37ed756b906c4a7eb5e91db8d25ffc1b212e
janaite <janaite@gmail.com>
2021-11-23 17:56:54 -0300

    trying to fix layout issues

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -4,42 +4,40 @@ import './styles.css';
 
 const Catalog = () => {
   return (
-    <div className="catalog-container">
-      <div className="container my-4">
-        <div className="catalog-search-container">
-          <input type="text" placeholder="Digite sua busca"/>
+    <div className="catalog-container container">
+      <div className="catalog-search-container row">
+        <div className="col-12">
+          <input type="text" placeholder="Digite sua busca" />
           <button className="btn btn-primary">Buscar</button>
         </div>
-        <div className="catalog-card-container">
-          <div className="row">
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-              <ProductCard />
-            </div>
-          </div>
+      </div>
+      <div className="catalog-card-container row">
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
+        <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
         </div>
       </div>
     </div>
